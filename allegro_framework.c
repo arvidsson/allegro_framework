bc457620ca7dc7fc8d206dd59b36606f13a2bf04
arvidsson <arvidsson@gmail.com>
2013-10-10 17:10:33 +0200

    Minor polish

--- allegro_framework.c
+++ allegro_framework.c
@@ -4,26 +4,26 @@
 #include <string.h>
 #include <time.h>
 
-ALLEGRO_EVENT_QUEUE *event_queue = NULL;
-ALLEGRO_DISPLAY *display = NULL;
-ALLEGRO_TIMER *timer = NULL;
-ALLEGRO_FILE *logfile = NULL;
-ALLEGRO_FONT *default_font = NULL;
-ALLEGRO_BITMAP *buffer = NULL;
-int scale_w, scale_h, scale_x, scale_y;
+static ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+static ALLEGRO_DISPLAY *display = NULL;
+static ALLEGRO_TIMER *timer = NULL;
+static ALLEGRO_FILE *logfile = NULL;
+static ALLEGRO_FONT *default_font = NULL;
+static ALLEGRO_BITMAP *buffer = NULL;
+static int scale_w, scale_h, scale_x, scale_y;
 
-bool done = false;
-bool paused = false;
-bool alt_tab_will_pause = true;
+static bool is_done = false;
+static bool is_paused = false;
+static bool should_alt_tab_pause = true;
 
-bool keys[ALLEGRO_KEY_MAX] = { false };
-bool keys_pressed[ALLEGRO_KEY_MAX] = { false };
-bool keys_released[ALLEGRO_KEY_MAX] = { false };
-int mouse_x = 0, mouse_y = 0;
-int mouse_dx = 0, mouse_dy = 0;
-bool mouse_buttons[MAX_MOUSE_BUTTONS] = { false };
-bool mouse_buttons_pressed[MAX_MOUSE_BUTTONS] = { false };
-bool mouse_buttons_released[MAX_MOUSE_BUTTONS] = { false };
+static bool keys[ALLEGRO_KEY_MAX] = { false };
+static bool keys_pressed[ALLEGRO_KEY_MAX] = { false };
+static bool keys_released[ALLEGRO_KEY_MAX] = { false };
+static int mouse_x = 0, mouse_y = 0;
+static int mouse_dx = 0, mouse_dy = 0;
+static bool mouse_buttons[MAX_MOUSE_BUTTONS] = { false };
+static bool mouse_buttons_pressed[MAX_MOUSE_BUTTONS] = { false };
+static bool mouse_buttons_released[MAX_MOUSE_BUTTONS] = { false };
 
 ALLEGRO_COLOR black_color;
 ALLEGRO_COLOR white_color;
@@ -49,68 +49,50 @@ void write_logfile(int log_level, const char *format, ...)
         exit(1);
 }
 
-void init_framework(const char *window_title, int display_width, int display_height, bool fullscreen)
+void init_framework(const char *window_title, int window_width, int window_height, bool fullscreen)
 {
     logfile = al_fopen("log.txt", "w");
     
-    if (!al_init()) {
+    if (!al_init())
         log_error("Failed to initialize allegro");
-    }
     
-    if (!al_install_keyboard()) {
+    if (!al_install_keyboard())
         log_error("Failed to install keyboard");
-    }
     
-    if (!al_install_mouse()) {
+    if (!al_install_mouse())
         log_error("Failed to install mouse");
-    }
     
-    if (!al_init_primitives_addon()) {
+    if (!al_init_primitives_addon())
         log_error("Failed to init primitives addon");
-    }
     
-    if (!al_init_image_addon()) {
+    if (!al_init_image_addon())
         log_error("Failed to init image addon");
-    }
     
     al_init_font_addon();
     default_font = al_create_builtin_font();
-    if (!default_font) {
+    if (!default_font)
         log_error("Failed to create builtin font");
-    }
     
     event_queue = al_create_event_queue();
-    if (!event_queue) {
+    if (!event_queue)
         log_error("Failed to create event queue");
-    }
     
-    if (fullscreen) {
-        al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW | ALLEGRO_OPENGL);
-    }
-    else {
-        al_set_new_display_flags(ALLEGRO_WINDOWED | ALLEGRO_OPENGL);
-    }
+    if (fullscreen)
+        al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
+    else
+        al_set_new_display_flags(ALLEGRO_WINDOWED);
     
-    al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
-    al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+    //al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+    //al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
     
-    ALLEGRO_DISPLAY_MODE disp_data;
-    al_get_display_mode(al_get_num_display_modes() - 1, &disp_data);
-    int midx = disp_data.width / 2;
-    int midy = disp_data.height / 2;
-    
-    al_set_new_window_position(midx - (display_width / 2), midy - (display_height / 2));
-    
-    display = al_create_display(display_width, display_height);
-    if (!display) {
-        log_error("Failed to create display @ %dx%d", display_width, display_height);
-    }
+    display = al_create_display(window_width, window_height);
+    if (!display)
+        log_error("Failed to create display @ %dx%d", window_width, window_height);
     al_set_window_title(display, window_title);
     
     timer = al_create_timer(1.0 / 60);
-    if (!timer) {
+    if (!timer)
         log_error("Failed to create timer");
-    }
     
     al_register_event_source(event_queue, al_get_keyboard_event_source());
     al_register_event_source(event_queue, al_get_mouse_event_source());
@@ -127,35 +109,29 @@ void init_framework(const char *window_title, int display_width, int display_hei
 
 void destroy_framework()
 {
-    if (default_font) {
+    if (default_font)
         al_destroy_font(default_font);
-    }
     
-    if (timer) {
+    if (timer)
         al_destroy_timer(timer);
-    }
     
-    if (display) {
+    if (display)
         al_destroy_display(display);
-    }
     
-    if (event_queue) {
+    if (event_queue)
         al_destroy_event_queue(event_queue);
-    }
     
-    if (logfile) {
+    if (logfile)
         al_fclose(logfile);
-    }
 }
 
 void setup_viewport(int width, int height, bool use_buffer_bitmap)
 {
     if (use_buffer_bitmap) {
         buffer = al_create_bitmap(width, height);
-        if (!buffer) {
+        if (!buffer)
             log_error("Failed to create buffer bitmap @ %dx%d", width, height);
-        }
-            
+        
         int window_width = al_get_display_width(display);
         int window_height = al_get_display_height(display);
         
@@ -187,24 +163,23 @@ void setup_viewport(int width, int height, bool use_buffer_bitmap)
 
 void alt_tab_should_pause(bool yesno)
 {
-    alt_tab_will_pause = yesno;
+    should_alt_tab_pause = yesno;
 }
 
 void run_game_loop(void (*update_proc)(), void (*draw_proc)())
 {
-    bool redraw = true;
+    bool should_redraw = true;
     al_start_timer(timer);
     
-    while (!done) {
+    while (!is_done) {
         ALLEGRO_EVENT event;
         al_wait_for_event(event_queue, &event);
         
         switch (event.type) {
             case ALLEGRO_EVENT_TIMER:
-                redraw = true;
-                if (!paused) {
+                should_redraw = true;
+                if (!is_paused)
                     update_proc();
-                }
                 memset(keys_pressed, false, sizeof(keys_pressed));
                 memset(keys_released, false, sizeof(keys_pressed));
                 memset(mouse_buttons_pressed, false, sizeof(mouse_buttons_pressed));
@@ -247,27 +222,26 @@ void run_game_loop(void (*update_proc)(), void (*draw_proc)())
                 break;
             
             case ALLEGRO_EVENT_DISPLAY_CLOSE:
-                done = true;
+                is_done = true;
                 break;
             
             case ALLEGRO_EVENT_DISPLAY_SWITCH_OUT:
-                if (alt_tab_will_pause) {
+                if (should_alt_tab_pause)
                     paused = true;
-                }
                 break;
             
             case ALLEGRO_EVENT_DISPLAY_SWITCH_IN:
-                if (alt_tab_will_pause) {
+                if (should_alt_tab_pause)
                     paused = false;
-                }
                 break;
         }
         
-        if (redraw && al_is_event_queue_empty(event_queue) && !paused) {
-            redraw = false;
-            if (buffer) {
+        if (should_redraw && al_is_event_queue_empty(event_queue) && !is_paused) {
+            should_redraw = false;
+            if (buffer)
                 al_set_target_bitmap(buffer);
-            }
+            else
+                al_set_target_bitmap(al_get_backbuffer(display));
             al_clear_to_color(al_map_rgb(0, 0, 0));
             
             draw_proc();
@@ -285,7 +259,7 @@ void run_game_loop(void (*update_proc)(), void (*draw_proc)())
 
 void quit()
 {
-    done = true;
+    is_done = true;
 }
 
 int get_window_width()
