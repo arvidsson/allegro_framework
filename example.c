8506c80135b2ecfc31816c7448e4c4959ced2d18
arvidsson <arvidsson@gmail.com>
2014-06-27 20:43:14 +0200

    Updated example

--- example.c
+++ example.c
@@ -15,20 +15,20 @@ void update()
     if (is_key_down(ALLEGRO_KEY_ESCAPE)) {
         quit();
     }
-    
+
     r.x += r.dx;
     r.y += r.dy;
-    
+
     if (r.x < 0 || r.x + r.w > get_viewport_width()) {
         r.dx = -r.dx;
     }
-    
+
     if (r.y < 0 || r.y + r.h > get_viewport_height()) {
         r.dy = -r.dy;
     }
 }
 
-void draw()
+void render()
 {
     al_draw_filled_rectangle(r.x, r.y, r.x + r.w, r.y + r.h, al_map_rgb(255, 0, 0));
 }
@@ -37,18 +37,9 @@ int main()
 {
     /* must be called first! */
     init_framework("example", 640, 480, false);
-    
-    /* optional, setup viewport to achieve resolution independence */
-    setup_viewport(320, 240, false);
-    
-    /* optional, setup alt-tab behavior (true by default) */
-    alt_tab_should_pause(false);
-    
+
     /* the game loop runs until we call quit() */
-    run_game_loop(update, draw);
-    
-    /* optional, called automatically when program ends */
-    destroy_framework();
-    
+    run_game_loop(update, render);
+
     return 0;
 }
